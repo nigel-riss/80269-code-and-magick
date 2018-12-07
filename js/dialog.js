f9ee374efbf3764b329c7d83b7b94f52c3775409
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2018-12-07 07:47:56 +0200

    Add drag-n-drop for setup window

--- js/dialog.js
+++ js/dialog.js
@@ -1,1 +1,60 @@
 'use strict';
+
+var setup = document.querySelector('.setup');
+setup.classList.remove('hidden');
+
+// var handle = document.querySelector('.setup-user-pic');
+var handle = document.querySelector('.upload');
+var startCoords = {x: null, y: null};
+var shift = {x: null, y: null};
+var isDragged = false;
+
+var onMouseDown = function (evt) {
+  evt.preventDefault();
+
+  startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+};
+
+var onMouseMove = function (evt) {
+  evt.preventDefault();
+
+  isDragged = true;
+
+  shift = {
+    x: startCoords.x - evt.clientX,
+    y: startCoords.y - evt.clientY
+  };
+
+  startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  setup.style.top = (setup.offsetTop - shift.y) + 'px';
+  setup.style.left = (setup.offsetLeft - shift.x) + 'px';
+};
+
+var onMouseUp = function (evt) {
+  evt.preventDefault();
+
+  document.removeEventListener('mousemove', onMouseMove);
+  document.removeEventListener('mouseup', onMouseUp);
+
+  if (isDragged) {
+    handle.addEventListener('click', onClickPreventDefault);
+    isDragged = false;
+  }
+};
+
+var onClickPreventDefault = function (evt) {
+  evt.preventDefault();
+  handle.removeEventListener('click', onClickPreventDefault);
+};
+
+handle.addEventListener('mousedown', onMouseDown);
